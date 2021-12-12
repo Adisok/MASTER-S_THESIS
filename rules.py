79189578fedb1f0f5eebaf9fe9ce5d6ce1f6ca34
Adam Sokolowski <adam.sokolowski@yougov.com>
2021-12-12 16:42:45 +0100

    Seems like it's working for now :x

--- rules.py
+++ rules.py
@@ -1,138 +1,168 @@
 from enum import Enum
 from itertools import product
 
-def rule_1(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
-    return f_pi == expected
+def rule_1(expected, fp, fk):
+    rule_check = [True if expected[i] == fp[i] else False for i in range(len(expected))]
+    if False in rule_check:
+        return False
+    return True
 
 
-def rule_2(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+
+def rule_2(expected, fp, fk):
     pass
-    return f_pi * (not(f_ki))
+    #return f_pi * (not(f_ki))
 
 
-def rule_3(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_3(expected, fp, fk):
     pass
-    return f_pi * J_j
+    #return f_pi * J_j
 
 
-def rule_4(expected=0, f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+# "Y": [0, 1, 1, 0, 0],
+# "fp": [0, 1, 1, 0, 0],
+# "fk": [0, 0, 0, 1, 0],
+def rule_4(expected, fp, fk):
     value = lambda f_ki, J_j: (not(f_ki)) * J_j
-    if expected != value(f_ki, J_j):
-        J_j = 1
-        return expected == value(f_ki, J_j)
-    else:
-        return expected == value(f_ki, J_j)
+    bool_check = True
 
-def rule_5(expected=0, f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+    count_0 = fk.count(0)
+    count_1 = fk.count(1)
+    for i in range(len(expected)):
+        if count_0:
+            J_j = 0
+            lol = value(fk[i], J_j)
+        if expected[i] == value(fk[i], J_j):
+            count_0 -= 1
+            pass
+        else:
+            if count_1:
+                J_j = 1
+            if expected[i] == value(fk[i], J_j):
+                count_1 -= 1
+                pass
+            else:
+                bool_check = False
+    if bool_check is False:
+        return bool_check
+    return True
+
+def rule_5(expected, fp, fk):
     value = lambda f_pi,y_i,f_ki: (f_pi + y_i) * (not(f_ki))
-    if expected != value(f_pi,y_i,f_ki):
-        y_i = 1
-        return value(f_pi,y_i,f_ki) == expected
-    else:
-        return True
+    bool_check = True
+    for i in range(len(expected)):
+        if 0 in fp:
+            y_i = 0
+        if expected[i] == value(fp[i], fk[i], expected[i]):
+            pass
+        else:
+            bool_check = False
+            break
+    for i in range(len(expected)):
+        if 1 in fp:
+            y_i = 1
+        if expected[i] == value(fp[i], fk[i], expected[i]):
+            pass
+        else:
+            return False
+    return bool_check
 
-def rule_6(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+
+def rule_6(expected, fp, fk):
     pass
-    return f_pi * (not(f_ki)) * J_j
+    #return f_pi * (not(f_ki)) * J_j
 
 
-def rule_7(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_7(expected, fp, fk):
     pass
-    return f_pi * ((not(f_ki)) * J_j)
+    #return f_pi * ((not(f_ki)) * J_j)
 
 
-def rule_8(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_8(expected, fp, fk):
     pass
-    return (f_pi + y_i) * ((not(f_ki)) + J_j)
+    #return (f_pi + y_i) * ((not(f_ki)) + J_j)
 
 
-def rule_9(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_9(expected, fp, fk):
     pass
-    return (f_pi + y_i) * (not(f_ki)) * J_j
+    #return (f_pi + y_i) * (not(f_ki)) * J_j
 
 
-def rule_10(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_10(expected, fp, fk):
     value = lambda f_pi,J_j,y_i,f_ki: (f_pi * J_j + y_i) * (not(f_ki))
-    for i in range(2):
-        for j in range(2):
-            if f_pi == 2:
-                for k in range(2):
-                    if expected != value(k,i,j,f_ki):
-                        return value == expected
-    return False
-
-
-
-def rule_11(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
     pass
-    return (f_pi * J_j + y_i) * (not(f_ki)) * J_k
 
 
-def rule_12(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+
+def rule_11(expected, fp, fk):
     pass
-    return (f_pi * J_j + y_i) * ((not(f_ki)) + J_k)
+    #return (f_pi * J_j + y_i) * (not(f_ki)) * J_k
 
 
-def rule_13(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_12(expected, fp, fk):
+    pass
+    #return (f_pi * J_j + y_i) * ((not(f_ki)) + J_k)
+
+
+def rule_13(expected, fp, fk):
     return False
-    return (f_pi * (not(m)), (f_ki + m) * J_j)
+    #return (f_pi * (not(m)), (f_ki + m) * J_j)
 
 
-def rule_14(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_14(expected, fp, fk):
     return False
+    #return (f_pi * J_j * (not(m)), (f_ki + m) * J_k)
 
-    return (f_pi * J_j * (not(m)), (f_ki + m) * J_k)
 
-
-def rule_15(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_15(expected, fp, fk):
     return False
-    return (f_pi * (not(m)), (f_ki * J_j + m) * J_k)
+    #return (f_pi * (not(m)), (f_ki * J_j + m) * J_k)
 
 
-def rule_16(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_16(expected, fp, fk):
     return False
-    return ((f_pi + y_i) * (not(m)), (f_ki + m) * J_j)
+    #return ((f_pi + y_i) * (not(m)), (f_ki + m) * J_j)
 
 
-def rule_17(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_17(expected, fp, fk):
     return False
-    return ((f_pi + y_i) * (not(m)), (f_ki * J_j + m ) * J_k)
+    #return ((f_pi + y_i) * (not(m)), (f_ki * J_j + m ) * J_k)
 
 
-def rule_18(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_18(expected, fp, fk):
     return False
-    return (f_pi * J_j * (not(m)), (f_ki * J_k + m) * J_l)
+    #return (f_pi * J_j * (not(m)), (f_ki * J_k + m) * J_l)
 
 
-def rule_19(expected=0,f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
+def rule_19(expected, fp, fk):
     return False
-    return ((f_pi * J_j + y_i) * (not(m)), (f_ki + m) * J_k)
+    #return ((f_pi * J_j + y_i) * (not(m)), (f_ki + m) * J_k)
 
 
 def rule_20(expected=0, f_pi=0, J_j=0, y_i=0, m=0, f_ki=0, J_k=0, J_l=0):
     value = lambda f_pi,J_j,y_i,m,J_k,J_l,f_ki: [((f_pi * J_j + y_i) * (not(m))), (f_ki * J_k + m) * J_l]
 
-    perm = list(product([0, 1], repeat=7))
-    if f_pi == 2 and f_ki == 2:
-        for i in perm:
-            val = value(i[0], i[1], i[2], i[3], i[4], i[5], i[6])
-            if expected == val[0] or expected == val[1]:
-                return True
-    elif f_pi == 2:
-        for i in perm:
-            val = value(i[0], i[1], i[2], i[3], i[4], i[5], f_ki)
-            if expected == val[0] or expected == val[1]:
-                return True
-    elif f_ki == 2:
-        for i in perm:
-            val = value(f_pi, i[1], i[2], i[3], i[4], i[5], i[6])
-            if expected == val[0] or expected == val[1]:
-                return True
-    else:
-        for i in perm:
-            val = value(f_pi, i[1], i[2], i[3], i[4], i[5], f_ki)
-            if expected == val[0] or expected == val[1]:
-                return True
+    # perm = list(product([0, 1], repeat=7))
+    # if f_pi == 2 and f_ki == 2:
+    #     for i in perm:
+    #         val = value(i[0], i[1], i[2], i[3], i[4], i[5], i[6])
+    #         if expected == val[0] or expected == val[1]:
+    #             return True
+    # elif f_pi == 2:
+    #     for i in perm:
+    #         val = value(i[0], i[1], i[2], i[3], i[4], i[5], f_ki)
+    #         if expected == val[0] or expected == val[1]:
+    #             return True
+    # elif f_ki == 2:
+    #     for i in perm:
+    #         val = value(f_pi, i[1], i[2], i[3], i[4], i[5], i[6])
+    #         if expected == val[0] or expected == val[1]:
+    #             return True
+    # else:
+    #     for i in perm:
+    #         val = value(f_pi, i[1], i[2], i[3], i[4], i[5], f_ki)
+    #         if expected == val[0] or expected == val[1]:
+    #             return True
     return False
 
 
--- test.py
+++ test.py
@@ -14,19 +14,14 @@ class TestRules:
             "fp": [0, 1, 1, 0, 0],
             "fk": [0, 0, 0, 1, 0],
         }
-        for i in range(1, 6):
-            rozwiazanie = []
-            for j in range(len(test_values["Y"])):
-                print(Rules[i](expected=test_values["Y"][j], f_pi=test_values["fp"][j], f_ki=test_values["fk"][j]))
-                rozwiazanie.append(
-                    Rules[i](expected=test_values["Y"][j], f_pi=test_values["fp"][j], f_ki=test_values["fk"][j]))
-                if isinstance(rozwiazanie[-1], tuple):
-                    rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"][j]
-            if False in rozwiazanie:
-                continue
-            else:
-                print(i)
-                print(Rules[i].__name__)
+        rozwiazanie = []
+
+        for i in range(1, 21):
+            if Rules[i](test_values["Y"], test_values["fp"], test_values["fk"]):
+                rozwiazanie.append(Rules[i].__name__)
+            # if isinstance(rozwiazanie[-1], tuple):
+            #     rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"]
+        print(rozwiazanie)
 
     def test_for_rule_five_being_used(self):
         test_values = {
@@ -34,53 +29,53 @@ class TestRules:
              "fp": [0, 1, 0, 0, 0],
              "fk": [0, 0, 0, 1, 0],
         }
-        for i in range(1, 6):
-            rozwiazanie = []
-            for j in range(len(test_values["Y"])):
-                print(Rules[i](expected=test_values["Y"][j], f_pi=test_values["fp"][j], f_ki=test_values["fk"][j]))
-                rozwiazanie.append(Rules[i](expected=test_values["Y"][j], f_pi=test_values["fp"][j], f_ki=test_values["fk"][j]))
-                if isinstance(rozwiazanie[-1], tuple):
-                    rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"][j]
-            if False in rozwiazanie:
-                continue
-            else:
-                print(i)
-                print(Rules[i].__name__)
+        rozwiazanie = []
 
-
-
-    def test_for_rule_ten_and_twenty_being_used(self):
-        test_values = {
-            "Y": [0, 0, 1, 1, 0],
-             "fp": [2, 1, 0, 0, 0],
-             "fk": [2, 0, 0, 1, 0],
-        }
         for i in range(1, 21):
-            rozwiazanie = []
-            for j in range(len(test_values["Y"])):
-                rozwiazanie.append(Rules[i](expected=test_values["Y"][j], f_pi=test_values["fp"][j], f_ki=test_values["fk"][j]))
-                if isinstance(rozwiazanie[-1], tuple):
-                    rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"][j]
-            if False in rozwiazanie:
-                continue
-            else:
-                print(Rules[i].__name__)
+            if Rules[i](test_values["Y"], test_values["fp"], test_values["fk"]):
+                rozwiazanie.append(Rules[i].__name__)
+            # if isinstance(rozwiazanie[-1], tuple):
+            #     rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"]
+        print(rozwiazanie)
 
+
+
+    # def test_for_rule_ten_and_twenty_being_used(self):
+    #     test_values = {
+    #         "Y": [0, 0, 1, 1, 0],
+    #          "fp": [2, 1, 0, 0, 0],
+    #          "fk": [2, 0, 0, 1, 0],
+    #     }
+    #     rozwiazanie = []
+    #
+    #     for i in range(1, 21):
+    #         if Rules[i](test_values["Y"], test_values["fp"], test_values["fk"]):
+    #             rozwiazanie.append(Rules[i].__name__)
+    #         # if isinstance(rozwiazanie[-1], tuple):
+    #         #     rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"]
+    #     print(rozwiazanie)
 
     def test_for_rule_one_and_four(self):
         test_values = {
-            "Y": [0, 0, 1, 1, 1],
+            "Y": [0, 1, 1, 0, 0],
              "fp": [0, 1, 1, 0, 0],
              "fk": [0, 0, 0, 1, 1],
         }
+        rozwiazanie = []
+
         for i in range(1, 21):
-            rozwiazanie = []
-            for j in range(len(test_values["Y"])):
-                rozwiazanie.append(Rules[i](expected=test_values["Y"][j], f_pi=test_values["fp"][j], f_ki=test_values["fk"][j]))
-                if isinstance(rozwiazanie[-1], tuple):
-                    rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"][j]
-            if False in rozwiazanie:
-                continue
-            else:
-                print(Rules[i].__name__)
-            print("4 i 1")
+            if Rules[i](test_values["Y"], test_values["fp"], test_values["fk"]):
+                rozwiazanie.append(Rules[i].__name__)
+            # if isinstance(rozwiazanie[-1], tuple):
+            #     rozwiazanie[-1] = rozwiazanie[-1][0] * rozwiazanie[-1][1] == test_values["Y"]
+        print(rozwiazanie)
+
+
+            # KUURWA LICZ NAJPIERW RULE DLA CALEJ LISTY DLA JEDNEJ ZMIENNEJ POMOCNICZEJ
+            # POTEM JAK NIE PYKNIE TO LICZ TO KURWA DLA DRUGIEJ ZMIENNEJ POMOCNICZEJ
+            # ALE ZEBY CIE KURWA NIE PODKUSILO ROBIC JEGO JEDNOSTKOWO TYLKO PIERDOLISZ
+            # PO CALOSCI Z OSOBNA DLA KAZDEJ ZMIENNEJ POMOCNICZEJ KUUUUUURWAAAAA
+            # CZYLI F(YI) = NOT(F_KI) * J DLA J = 0
+            # I LECIMY DLA fk[0](J=0) potem fk[1](J=0) i tak do konca i jak nie pyknie
+            # TO LECISZMY TAK fk[0](J=1) potem fk[1](J=1) i tak dokonca i wtedy jak nie pyknie
+            # to lecimy next i chuj
