41012038b3982b7fc32f5fe4ead8c1c3bd15e95c
Adam Sokolowski <adam.sokolowski@yougov.com>
2022-01-02 20:18:31 +0100

    Add drawer

--- GUI/custom_buttons.py
+++ GUI/custom_buttons.py
@@ -1,8 +1,8 @@
 import sys
 
-from PyQt5.QtCore import Qt, QMimeData, QDataStream, QByteArray, QIODevice, QPoint
-from PyQt5.QtGui import QDrag, QIcon, QPixmap
-from PyQt5.QtWidgets import QPushButton, QWidget, QApplication
+from PyQt5.QtCore import Qt, QMimeData, QDataStream, QByteArray, QIODevice, QPoint, QPointF
+from PyQt5.QtGui import QDrag, QIcon, QPixmap, QPainterPath
+from PyQt5.QtWidgets import QPushButton, QWidget, QApplication, QGraphicsScene
 
 
 class Button(QPushButton):
@@ -12,7 +12,7 @@ class Button(QPushButton):
         self.title = title
         self.setFixedHeight(100)
         self.setFixedWidth(100)
-        self.setStyleSheet(f"background-image : url(images/test.jpg);")
+        self.setStyleSheet(f"background-image : url(GUI/images/test.jpg);")
 
     def mouseMoveEvent(self, e):
 
@@ -28,41 +28,3 @@ class Button(QPushButton):
         mimeData.setData('myApp/QtWidget', byteArray)
 
         dropAction = drag.exec_(Qt.MoveAction)
-
-
-class Example(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        self.setAcceptDrops(True)
-
-        self.button = Button('Button')
-        self.button.move(100, 65)
-
-        self.setWindowTitle('Click or Move')
-        self.setGeometry(300, 300, 550, 450)
-
-    def dragEnterEvent(self, e):
-        e.accept()
-
-    def dropEvent(self, e):
-        position = e.pos()
-        self.button.move(position)
-
-        e.setDropAction(Qt.MoveAction)
-        e.accept()
-
-
-def main():
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    app.exec_()
-
-
-if __name__ == '__main__':
-    main()
---
+++ GUI/drawer.py
@@ -0,0 +1,87 @@
+import sys
+from PyQt5.QtCore import Qt, QPoint, QRect, QLine
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtGui import QPixmap, QPainter, QPen, QImage, QColor
+from numpy import ones,vstack
+from numpy.linalg import lstsq
+
+
+class Drawer(QWidget):
+
+    def __init__(self, parent=None):
+        super(Drawer, self).__init__(parent)
+        self.lines = dict()
+        self.firstPoint = self.lastPoint = None
+        self.image = QImage(self.size(), QImage.Format_RGB32)
+        self.image.fill(Qt.white)
+        self.setGeometry(100, 100, 500, 300)
+        self.resize(self.image.width(), self.image.height())
+        self.j = 0
+        self.show()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.firstPoint = event.pos()
+        if event.button() == Qt.RightButton:
+            mouse_pos = event.pos()
+            for i in range(len(self.lines)):
+                if mouse_pos.y() > self.lines[i][1](x=mouse_pos.x())+10 \
+                        or mouse_pos.y() > self.lines[i][1](x=mouse_pos.x())-10:
+                    try:
+                        self.lines[i] = self.lines[i+1]
+                    except KeyError:
+                        del self.lines[i]
+            self.image = QImage(self.size(), QImage.Format_RGB32)
+            self.image.fill(Qt.white)
+            self.updateImage()
+
+    def mouseMoveEvent(self, event):
+        if self.firstPoint:
+            self.lastPoint = event.pos()
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        if self.firstPoint and self.lastPoint:
+            self.updateImage()
+
+    def updateImage(self):
+        if self.firstPoint and self.lastPoint:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
+            self._line = QLine(self.firstPoint, self.lastPoint)
+            p1 = self._line.p1()
+            p2 = self._line.p2()
+            points = [(p1.x(), p1.y()), (p2.x(), p2.y())]
+            x_coords, y_coords = zip(*points)
+            A = vstack([x_coords, ones(len(x_coords))]).T
+            a, b = lstsq(A, y_coords, rcond=None)[0]
+            function = lambda x, a=a, b=b: a*x + b
+            self.lines[self.j] = [self._line, function]
+            self.j += 1
+            painter.drawLine(self._line)
+            painter.end()
+            self.firstPoint = self.lastPoint = None
+            self.update()
+        else:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
+            for i in self.lines.values():
+                painter.drawLine(i[0])
+            painter.end()
+            self.j = len(self.lines)
+            self.firstPoint = self.lastPoint = None
+            self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
+        dirtyRect = event.rect()
+        painter.drawImage(dirtyRect, self.image, dirtyRect)
+        if self.firstPoint and self.lastPoint:
+            painter.drawLine(self.firstPoint, self.lastPoint)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainMenu = Drawer()
+    mainMenu.show()
+    sys.exit(app.exec_())
